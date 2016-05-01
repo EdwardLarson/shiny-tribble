7dde58e3c934698052c4a047ff302c733e6c7b03
Ed Larson <wasabiofip@gmai.com>
2016-05-01 14:41:50 -0400

    Linked SDL2 libraries

---
+++ shiny-tribble/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char* argv[]) {
+	std::cout << "Please input any number" << std::endl;
+	int n;
+	std::cin >> n;
+	std::cout << "Your number is " << n << std::endl;
+
+	return 0;
+}
